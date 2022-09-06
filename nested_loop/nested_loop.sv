550c0a3a43ac855f922c9b05b324456aec77480e
piyushagrawal@mirafra.com <piyushagrawal@mirafra.com>
2022-09-07 03:54:26 +0530

    done

--- nested_loop/nested_loop.sv
+++ nested_loop/nested_loop.sv
@@ -11,37 +11,23 @@
 //--------------------------------------------------------------------------------------------
 
 module nested_loop;
-  //event stop;
   initial 
   begin
     $display("\t ----nested loop output ---\n");
     nes();
-    /*if(stop.triggered)
-      begin
-        $display("....");
-        disable nes;
-      end*/
   end
 
   task nes();
-    //fork
       for (int i=1;i<=3;i++)
       begin
         for(int j=1;j<=3;j++)
         begin
           if(i==2)
           begin
-            //->stop;
             disable nes;
-            //break;
           end
           $display("\t i= %0d , j= %0d ",i,j);
         end
       end
-    //join
     endtask
-
-        //break;
-    //end
-
 endmodule:nested_loop
